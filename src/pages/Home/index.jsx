0e40bd42d0633a9db84f0dbf6750f93c1c1ca810
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2021-07-11 12:51:56 -0300

    update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
-import Container from './styles';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
-const Home = () => <Container>Hello World</Container>;
+import { Wrapper, Container, Search, Logo, Map } from './styles';
+import logo from '../../assets/logo.svg';
+
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="logo do restaurante" />
+          <TextField
+            label="Pesquise"
+            outlined
+            //onTrailingIconSelect={() => this.setState({ value: '' })}
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+          </TextField>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
+  );
+};
 
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,33 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
-  background-color: blue;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 
-export default Container;
+const Container = styled.aside`
+  background-color: ${(props) => props.theme.colors.background};
+  width: 360px;
+  height: 100vh;
+  overflow-y: auto;
+`;
+
+const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+  padding: 16px;
+`;
+
+const Logo = styled.img`
+  margin-bottom: 15px;
+  height: 50px;
+`;
+
+const Map = styled.div`
+  background-color: red;
+  width: 100vw;
+`;
+// scroll
+export { Wrapper, Container, Search, Logo, Map };
